f6c54229b413316e52d4721d025c6bdeffb3f287
Nusa Babic <nusa@grover.com>
2020-11-23 20:53:12 +0100

    Be sensible about element identifiers

--- puppet.js
+++ puppet.js
@@ -3,14 +3,13 @@ const puppeteer = require('puppeteer');
 const config = require('dotenv').config();
 
 (async() => {
-    const start = +new Date();
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto(process.env.URL, {"waitUntil" : "networkidle0"});
     await page.waitForNavigation();
-    await page.type('#email', process.env.USERNAME);
-    await page.type('#password', process.env.PASSWORD);
-    await page.click('#login-submit');
+    await page.type(process.env.USERNAME_ELEMENT_SELECTOR, process.env.USERNAME);
+    await page.type(process.env.PASSWORD_ELEMENT_SELECTOR, process.env.PASSWORD);
+    await page.click(process.env.SUBMIT_ELEMENT_SELECTOR);
     await page.waitForNavigation();
     const interval = setInterval(() => page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] }), process.env.INTERVAL)
 })();
