19b9c95c2e82b0e34f2775c35cdde511fd6a973d
Nusa Babic <nusa@grover.com>
2020-11-23 21:10:10 +0100

    larger viewport

--- puppet.js
+++ puppet.js
@@ -5,11 +5,12 @@ const config = require('dotenv').config();
 (async() => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
+    await page.setViewport({ width: 1366, height: 768});
     await page.goto(process.env.URL, {"waitUntil" : "networkidle0"});
     await page.waitForNavigation();
     await page.type(process.env.USERNAME_ELEMENT_SELECTOR, process.env.USERNAME);
     await page.type(process.env.PASSWORD_ELEMENT_SELECTOR, process.env.PASSWORD);
     await page.click(process.env.SUBMIT_ELEMENT_SELECTOR);
     await page.waitForNavigation();
-    const interval = setInterval(() => page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] }), process.env.INTERVAL)
+    const interval = setInterval(async () => await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] }), process.env.INTERVAL)
 })();
